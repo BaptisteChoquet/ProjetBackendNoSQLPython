ec3f6639aaa73851028646bbf843aa177adfce24
AxelDemorest <axel.demorest@gmail.com>
2021-10-12 15:02:43 +0200

    list pagination

--- python_api/main.py
+++ python_api/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 @app.route("/users", methods=['GET'])
-def hello_world():
+def list_users():
     path = 'users'
 
     files = os.listdir(path)
@@ -14,11 +14,35 @@ def hello_world():
         with open(f"users/{name}") as test:
             data = test.read()
             obj["users"][os.path.splitext(name)[0]] = {
-                    "firstname": data.split("\n")[0],
-                    "lastname": data.split("\n")[1]
-                 }
+                "firstname": data.split("\n")[0],
+                "lastname": data.split("\n")[1]
+            }
 
     return obj
+
+
+# arguments : num page et taille de la page
+@app.route("/list_users_page", methods=['GET'])
+def list_users_page():
+    path = 'users'
+
+    files = os.listdir(path)
+    # array = []
+    # for name in files:
+    #     with open(f"users/{name}") as test:
+    #         data = test.read()
+    #         array.append(data.split("\n")[0] + " " + data.split("\n")[1])
+
+    current_page = int(request.args.get("page"))
+    size_page = int(request.args.get("size"))
+    users_len = len(files)
+
+    total_page = users_len / current_page
+
+    test = files[(current_page-1) * size_page:((current_page-1) * size_page) + size_page]
+    print(test)
+
+    return make_response(" - ", 200)
 
 
 if __name__ == '__main__':
