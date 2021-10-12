490cff7f9c6c8061d49d16ef46a313e87ac0bde0
AxelDemorest <axel.demorest@gmail.com>
2021-10-12 15:37:00 +0200

    Finitions pagination

--- python_api/main.py
+++ python_api/main.py
@@ -27,22 +27,28 @@ def list_users_page():
     path = 'users'
 
     files = os.listdir(path)
-    # array = []
-    # for name in files:
-    #     with open(f"users/{name}") as test:
-    #         data = test.read()
-    #         array.append(data.split("\n")[0] + " " + data.split("\n")[1])
+
+    if not(request.args.get("page")):
+        return make_response("Page non indiquée", 404)
+
+    if not(request.args.get("size")):
+        return make_response("Taille de page non indiquée", 404)
 
     current_page = int(request.args.get("page"))
     size_page = int(request.args.get("size"))
-    users_len = len(files)
 
-    total_page = users_len / current_page
+    users_page = files[(current_page - 1) * size_page:((current_page - 1) * size_page) + size_page]
 
-    test = files[(current_page-1) * size_page:((current_page-1) * size_page) + size_page]
-    print(test)
+    obj = {"users": {}}
+    for name in users_page:
+        with open(f"users/{name}") as open_file:
+            data = open_file.read()
+            obj["users"][os.path.splitext(name)[0]] = {
+                "firstname": data.split("\n")[0],
+                "lastname": data.split("\n")[1]
+            }
 
-    return make_response(" - ", 200)
+    return obj
 
 
 if __name__ == '__main__':
